3f3fe81b52f1c402a13258fad89ae461fa4d5c0f
dev0xjosh <bsuyiajayi@gmail.com>
2026-03-04 00:49:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+import {
+  createWalletClient,
+  custom,
+  createPublicClient,
+  parseEther,
+  defineChain,
+} from "https://esm.sh/viem";
+import { contractAddress, abi } from "./constant.js";
+
+let walletClient;
+let publicClient;
+
+const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
+const ethAmountInput = document.getElementById("ethAmount");
+
+connectButton.onclick = connect;
+fundButton.onclick = fund;
+
+async function connect() {
+  if (typeof window.ethereum !== "undefined") {
+    walletClient = createWalletClient({
+      transport: custom(window.ethereum),
+    });
+    await walletClient.requestAddresses();
+    connectButton.innerHTML = "Connected";
+  } else {
+    connectButton.innerHTML = "Please install MetaMask!!!";
+  }
+}
+
+async function fund() {
+  const ethAmount = ethAmountInput.value;
+  console.log(`Funding with ${ethAmount}.....`);
+
+  if (typeof window.ethereum !== "undefined") {
+    walletClient = createWalletClient({
+      transport: custom(window.ethereum),
+    });
+    const [connectedAccount] = await walletClient.requestAddresses();
+    const currentChain = await getCurrentChain(walletClient);
+
+    publicClient = createPublicClient({
+      transport: custom(window.ethereum),
+    });
+    console.log(parseEther(ethAmount));
+    await publicClient.simulateTransaction({
+      address: contractAddress,
+      abi: abi,
+      functionName: "fund",
+      account: connectedAccount,
+      chain: currentChain,
+      value: parseEther(ethAmount),
+    });
+  } else {
+    connectButton.innerHTML = "Please install MetaMask!!!";
+  }
+}
+
+async function getCurrentChain(client) {
+  const chainId = await client.getChainId();
+  const currentChain = defineChain({
+    id: chainId,
+    name: "Custom Chain",
+    nativeCurrency: {
+      name: "Ether",
+      symbol: "ETH",
+      decimals: 18,
+    },
+    rpcUrls: {
+      default: {
+        http: ["http://localhost:8545"],
+      },
+    },
+  });
+  return currentChain;
+}
